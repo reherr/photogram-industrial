d28eebc5eb4c20e63cbf4f1c76ebe4037f41f686
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-07-02 20:44:58 +0000

    added feed, followers, following RCAV

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def feed
+    @user = current_user
+  end
+
+  def followers
+  end
+
+  def following
+  end
+  
 end
