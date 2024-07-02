dd9ca4e3b702cb85003af61cc417fc41979b94b0
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-07-02 19:15:05 +0000

    added users route to include username and users#show

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/:username" => "users#show", as: :user
+
 end
